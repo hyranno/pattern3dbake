5e85f4a25d4548a306de3b30fca9e21127603ddd
hyranno <hyranno4pub@gmail.com>
2023-02-23 10:25:18 +0900

    refactoring: removed deprecated canvas

--- src/App.tsx
+++ src/App.tsx
@@ -78,6 +78,9 @@ const App: Component = () => {
     });
     return scene;
   })(canvas_model_src);
+  babylon.SceneLoader.ImportMesh("",
+    ...breakUrl(sampleModelUrl), scene_model_src,
+  );
 
 
   let canvas_model = document.createElement("canvas");
@@ -94,23 +97,6 @@ const App: Component = () => {
     return scene;
   })(canvas_model);
 
-  let canvas_work = document.createElement("canvas");
-  canvas_work.width = 512;
-  canvas_work.height = 512;
-  let engine_work = new babylon.Engine(canvas_work, true);
-  engine_work.setViewport(new babylon.Viewport(0, 0, 1.0, 1.0));
-  let scene_work = new babylon.Scene(engine_work);
-  scene_work.createDefaultCameraOrLight();
-
-  let canvas_diffuse_src = document.createElement("canvas");
-
-  let canvas_diffuse = document.createElement("canvas");
-  let texture_diffuse = new babylon.DynamicTexture("diffuse", canvas_diffuse, scene_model);
-
-
-  babylon.SceneLoader.ImportMesh("",
-    ...breakUrl(sampleModelUrl), scene_model_src,
-  );
   babylon.SceneLoader.ImportMesh("",
     ...breakUrl(sampleModelUrl), scene_model,
     (meshes) => {
@@ -138,64 +124,6 @@ const App: Component = () => {
     },
   );
 
-  babylon.SceneLoader.ImportMesh("",
-    ...breakUrl(sampleModelUrl), scene_work,
-    (meshes) => {
-      {
-        let material = new babylon.ShaderMaterial("shader", scene_work, {
-          vertexSource: textureVertShader,
-          fragmentSource: copyFragShader,
-        }, {
-          attributes: ["position", "normal", "uv"],
-          uniforms: ["resolution"],
-        });
-        material.setTexture("src", (meshes[12].material!.clone("org") as babylon.PBRMaterial).albedoTexture!);
-        let plane = babylon.MeshBuilder.CreatePlane("plane", {}, scene_work);
-        material.depthFunction = babylon.Constants.ALWAYS;
-        plane.material = material;
-
-        let camera = new babylon.Camera("camera_diffuse_src", new babylon.Vector3(), scene_work);
-        let view = engine_work.registerView(canvas_diffuse_src, camera);
-
-        let render_once = () => {
-          if (engine_work.activeView! === view && plane.isReady(true)) {
-            engine_work.clear(new babylon.Color4(0), true, false);
-            plane.subMeshes.forEach(m => m.render(false));
-            engine_work.unRegisterView(canvas_diffuse_src);
-            engine_work.stopRenderLoop(render_once);
-          }
-        };
-        engine_work.runRenderLoop(render_once);
-      }
-      {
-        let target = meshes[12] as babylon.Mesh;
-        let material = new babylon.ShaderMaterial("shader", scene_work, {
-          vertexSource: textureVertShader,
-          fragmentSource: fbmNoiseFragShader,
-        }, {
-          attributes: ["position", "normal", "uv"],
-          uniforms: ["resolution"],
-        });
-        material.cullBackFaces = false;
-        material.depthFunction = babylon.Constants.ALWAYS;
-        target.material = material;
-
-        let camera = new babylon.Camera("camera_diffuse", new babylon.Vector3(), scene_work);
-        let view = engine_work.registerView(canvas_diffuse, camera);
-
-        let render_once = () => {
-          if (engine_work.activeView! === view && target.isReady(true)) {
-            engine_work.clear(new babylon.Color4(0), true, false);
-            target.subMeshes.forEach(m => m.render(false));
-            setTimeout(() => texture_diffuse.update()); // update after this rendering call
-            engine_work.unRegisterView(canvas_diffuse);
-            engine_work.stopRenderLoop(render_once);
-          }
-        };
-        engine_work.runRenderLoop(render_once);
-      }
-    }
-  );
 
   return (
     <div class={styles.App}>
@@ -204,10 +132,6 @@ const App: Component = () => {
       </div>
       <div>
         {canvas_model}
-      </div>
-      <div>
-        {canvas_diffuse_src}
-        {canvas_diffuse}
       </div>
     </div>
   );
